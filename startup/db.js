7cea150e6716a4a3bb3fee924c1aa6ca09a16027
David C <davidc@trottercontrols.com>
2020-03-23 01:05:11 +0000

    config stuff

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 module.exports = function() {
     const db = config.get('db');
-
+    console.log(db);
     mongoose.connect(db, 
         { 
             useNewUrlParser: true, 
